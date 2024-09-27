c14c70901ff1535e72c682e66138b0f58143e197
haxgerd <fassx844@gmail.com>
2024-09-27 22:50:16 +0700

    Update po.php

--- po.php
+++ po.php
@@ -13,32 +13,29 @@ function getFileRowCount($filename)
 
     return $rowCount;
 }
-$protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'https';
-$fullUrl = $protocol . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-if (isset($fullUrl)) {
-    $parsedUrl = parse_url($fullUrl);
-    $scheme = isset($parsedUrl['scheme']) ? $parsedUrl['scheme'] : '';
-    $host = isset($parsedUrl['host']) ? $parsedUrl['host'] : '';
-    $path = isset($parsedUrl['path']) ? $parsedUrl['path'] : '';
-    $baseUrl = $scheme . "://" . $host . $path;
-    $urlAsli = str_replace("po.php", "", $baseUrl);
-    $judulFile = "list.txt";
-    $jumlahBaris = getFileRowCount($judulFile);
-    $sitemapFile = fopen("sitemap.xml", "w");
-    fwrite($sitemapFile, '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL);
-    fwrite($sitemapFile, '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . PHP_EOL);
-    $fileLines = file($judulFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    foreach ($fileLines as $index => $judul) {
-        $sitemapLink = $urlAsli . '?tunnel=' . urlencode($judul);
-        fwrite($sitemapFile, '  <url>' . PHP_EOL);
-        fwrite($sitemapFile, '    <loc>' . $sitemapLink . '</loc>' . PHP_EOL);
-        fwrite($sitemapFile, '  </url>' . PHP_EOL);
-    }
-    fwrite($sitemapFile, '</urlset>' . PHP_EOL);
-    fclose($sitemapFile);
-    echo "sitemap berhasil dibuat";
-} else {
-    echo "URL saat ini tidak didefinisikan.";
+
+$protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http';
+$domain = $_SERVER['HTTP_HOST'];
+$dir = rtrim(dirname($_SERVER['PHP_SELF']), '/');
+$file = 'list.txt';
+$urlPrefix = '/';
+$urlSuffix = '.html';
+
+$jumlahBaris = getFileRowCount($file);
+$sitemapFile = fopen("sitemap.xml", "w");
+fwrite($sitemapFile, '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL);
+fwrite($sitemapFile, '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . PHP_EOL);
+
+$fileLines = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+foreach ($fileLines as $index => $judul) {
+    $sitemapLink = $protocol . '://' . $domain . $dir . $urlPrefix . urlencode($judul) . $urlSuffix;
+    fwrite($sitemapFile, '  <url>' . PHP_EOL);
+    fwrite($sitemapFile, '    <loc>' . $sitemapLink . '</loc>' . PHP_EOL);
+    fwrite($sitemapFile, '  </url>' . PHP_EOL);
 }
 
+fwrite($sitemapFile, '</urlset>' . PHP_EOL);
+fclose($sitemapFile);
+
+echo "Sitemap telah berhasil dibuat!";
 ?>
